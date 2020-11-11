5f442615469c745a2566be7c1f779cc8482c8970
Wangdaoshuai <51895873+Wangdaoshuai@users.noreply.github.com>
2020-11-11 08:39:01 -0600

    Add files via upload

---
+++ Opencv系统学习/项目实战/02文档扫描/test.py
@@ -0,0 +1,34 @@
+# https://digi.bib.uni-mannheim.de/tesseract/
+# 配置环境变量如E:\Program Files (x86)\Tesseract-OCR
+# tesseract -v进行测试
+# tesseract XXX.png 得到结果 
+# pip install pytesseract
+# anaconda lib site-packges pytesseract pytesseract.py
+# tesseract_cmd 修改为绝对路径即可
+# 用开源工具OCR识别字符
+from PIL import Image
+import pytesseract  # 开源ocr工具
+import cv2
+import os
+
+preprocess = 'blur' #thresh
+
+image = cv2.imread('scan.jpg')
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+if preprocess == "thresh":
+    gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+
+if preprocess == "blur":
+    gray = cv2.medianBlur(gray, 3)
+    
+filename = "{}.png".format(os.getpid())
+cv2.imwrite(filename, gray)
+    
+text = pytesseract.image_to_string(Image.open(filename))
+print(text)
+os.remove(filename)
+
+cv2.imshow("Image", image)
+cv2.imshow("Output", gray)
+cv2.waitKey(0)                                   
